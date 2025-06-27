534083c300e78234d3271567d89df9dd58a980e3
Arseniy <arseniy@MacBook-Air-Arseniy.local>
2025-06-27 15:20:14 +0300

    Добавлен кастомный модификатор onShake, улавливающий тряску телефона. Теперь при тряске телефона на экране баланса сумма скрывается эффектом анимированного спойлера (как в tg); Критерий 3 выполнен

--- Financify/Scenes/Balance/BalanceView.swift
+++ Financify/Scenes/Balance/BalanceView.swift
@@ -3,7 +3,8 @@ import SwiftUI
 struct BalanceView: View {
     // MARK: - Properties
     @State private var isEditing: Bool = false
-    @State private var showCurrencyDialog = false
+    @State private var showCurrencyDialog: Bool = false
+    @State private var isBalanceHidden: Bool = false
     @StateObject private var viewModel: BalanceViewModel
     
     @State private var editingTotalText: String = ""
@@ -17,83 +18,10 @@ struct BalanceView: View {
     var body: some View {
         NavigationStack {
             List {
-                Section {
-                    // По умолчанию List не умеет анимировать смену цвета через listRowBackground, поэтому вместо этого используется кастомный фон
-                    ZStack {
-                        (isEditing ? Color.white : Color.accent)
-                            .animation(.easeInOut(duration: Constants.Style.animationDuration), value: isEditing)
-                        
-                        HStack {
-                            Text(Constants.Text.balanceTitle)
-                            
-                            Spacer()
-                            
-                            if isEditing {
-                                TextField(
-                                    "",
-                                    text: $editingTotalText,
-                                    onCommit: commitTotalEdit
-                                )
-                                .keyboardType(.decimalPad)
-                                .multilineTextAlignment(.trailing)
-                                .focused($totalFieldIsFocused)
-                                .onAppear {
-                                    let raw = viewModel.total.moneyFormatted
-                                    let noSpaces = raw.components(separatedBy: .whitespacesAndNewlines).joined()
-                                    editingTotalText = noSpaces
-                                    totalFieldIsFocused = true
-                                }
-                            } else {
-                                Text("\(viewModel.total.moneyFormatted)")
-                                    .foregroundStyle(Color(hex: Constants.Style.primaryTextColorHex))
-                            }
-                            Text("\(viewModel.selectedCurrency.rawValue)")
-                                .foregroundStyle(Color(hex: Constants.Style.primaryTextColorHex))
-                        }
-                        .animation(nil, value: isEditing)
-                        .padding(.vertical, Constants.Style.verticalPadding)
-                        .padding(.horizontal)
-                    }
-                    .listRowInsets(.init())
-                }
-                
-                Section {
-                    ZStack {
-                        (isEditing ? Color.white : Color(hex: Constants.Style.currencySectionBackgroundHex))
-                            .animation(.easeInOut(duration: Constants.Style.animationDuration), value: isEditing)
-                        
-                        Button(action: {
-                            if isEditing {
-                                showCurrencyDialog = true
-                            }
-                        }) {
-                            HStack {
-                                Text(Constants.Text.currencyTitle).foregroundColor(.black)
-                                Spacer()
-                                Text(viewModel.selectedCurrency.rawValue)
-                                    .foregroundStyle(Color(hex: Constants.Style.primaryTextColorHex))
-                                if isEditing {
-                                    Image(systemName: Constants.SFSymbols.chevronRight).font(Font.system(.footnote).weight(.semibold)).tint(Color.gray.opacity(Constants.Style.chevronOpacity))
-                                }
-                            }
-                            .padding(.vertical, Constants.Style.verticalPadding)
-                            .padding(.horizontal)
-                            .contentShape(Rectangle())
-                        }
-                    }
-                    .listRowInsets(.init())
-                }
-                .confirmationDialog(Constants.Text.currencyTitle, isPresented: $showCurrencyDialog, titleVisibility: .visible) {
-                    ForEach(Currency.allCases, id: \.self) { currency in
-                        Button(currency.currencyTitle) {
-                            viewModel.selectedCurrency = currency
-                        }
-                    }
-                }
+                balanceSection
+                currencySection
             }
-            .refreshable {
-                await viewModel.refresh()
-            }
+            .refreshable {  await viewModel.refresh() }
             .scrollDismissesKeyboard(.immediately)
             .listSectionSpacing(Constants.Style.sectionSpacing)
             .safeAreaInset(edge: .top, spacing: 0) { // Отступ сверху List
@@ -102,17 +30,7 @@ struct BalanceView: View {
             .navigationTitle(Constants.Text.navigationTitle)
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: {
-                        if isEditing {
-                            withAnimation(.easeInOut(duration: Constants.Style.animationDuration)) {
-                                commitTotalEdit()
-                            }
-                        } else {
-                            withAnimation(.easeInOut(duration: Constants.Style.animationDuration)) {
-                                isEditing = true
-                            }
-                        }
-                    }) {
+                    Button(action: toggleEditMode) {
                         Image(systemName: isEditing ? Constants.SFSymbols.checkmark : Constants.SFSymbols.pencil)
                             .contentTransition(
                                 .symbolEffect(
@@ -129,6 +47,101 @@ struct BalanceView: View {
         .onTapGesture {
             if totalFieldIsFocused {
                 totalFieldIsFocused = false
+            }
+        }
+    }
+    
+    private var balanceSection: some View {
+        Section {
+            // По умолчанию List не умеет анимировать смену цвета через listRowBackground, поэтому вместо этого используется кастомный фон
+            ZStack {
+                (isEditing ? Color.white : Color.accent)
+                    .animation(.easeInOut(duration: Constants.Style.animationDuration), value: isEditing)
+                
+                HStack {
+                    Text(Constants.Text.balanceTitle)
+                    
+                    Spacer()
+                    
+                    if isEditing {
+                        TextField(
+                            "",
+                            text: $editingTotalText,
+                            onCommit: commitTotalEdit
+                        )
+                        .keyboardType(.decimalPad)
+                        .multilineTextAlignment(.trailing)
+                        .focused($totalFieldIsFocused)
+                        .onAppear {
+                            let raw = viewModel.total.moneyFormatted
+                            let noSpaces = raw.components(separatedBy: .whitespacesAndNewlines).joined()
+                            editingTotalText = noSpaces
+                            totalFieldIsFocused = true
+                        }
+                    } else {
+                        Text("\(viewModel.total.moneyFormatted)")
+                            .spoiler(isOn: $isBalanceHidden)
+                            .onShake {
+                                withAnimation(.easeIn(duration: Constants.Style.animationDuration)) {
+                                    isBalanceHidden.toggle()
+                                }
+                            }
+                            .foregroundStyle(Color(hex: Constants.Style.primaryTextColorHex))
+                    }
+                    Text("\(viewModel.selectedCurrency.rawValue)")
+                        .foregroundStyle(Color(hex: Constants.Style.primaryTextColorHex))
+                }
+                .animation(nil, value: isEditing)
+                .padding(.vertical, Constants.Style.verticalPadding)
+                .padding(.horizontal)
+            }
+            .listRowInsets(.init())
+        }
+    }
+    
+    private var currencySection: some View {
+        Section {
+            ZStack {
+                (isEditing ? Color.white : Color(hex: Constants.Style.currencySectionBackgroundHex))
+                    .animation(.easeInOut(duration: Constants.Style.animationDuration), value: isEditing)
+                
+                Button(action: {
+                    if isEditing {
+                        showCurrencyDialog = true
+                    }
+                }) {
+                    HStack {
+                        Text(Constants.Text.currencyTitle).foregroundColor(.black)
+                        Spacer()
+                        Text(viewModel.selectedCurrency.rawValue)
+                            .foregroundStyle(Color(hex: Constants.Style.primaryTextColorHex))
+                        if isEditing {
+                            Image(systemName: Constants.SFSymbols.chevronRight).font(Font.system(.footnote).weight(.semibold)).tint(Color.gray.opacity(Constants.Style.chevronOpacity))
+                        }
+                    }
+                    .padding(.vertical, Constants.Style.verticalPadding)
+                    .padding(.horizontal)
+                    .contentShape(Rectangle())
+                }
+            }
+            .listRowInsets(.init())
+        }
+        .confirmationDialog(Constants.Text.currencyTitle, isPresented: $showCurrencyDialog, titleVisibility: .visible) {
+            ForEach(Currency.allCases, id: \.self) { currency in
+                Button(currency.currencyTitle) {
+                    viewModel.selectedCurrency = currency
+                }
+            }
+        }
+    }
+    
+    // MARK: - Private Methods
+    private func toggleEditMode() {
+        withAnimation(.easeInOut(duration: Constants.Style.animationDuration)) {
+            if isEditing {
+                commitTotalEdit()
+            } else {
+                isEditing = true
             }
         }
     }
