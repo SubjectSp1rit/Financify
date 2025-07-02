cd1ded21651b69e8ab179ba4440d84a89427817b
Arseniy <arseniy@MacBook-Air-Arseniy.local>
2025-07-02 15:14:18 +0300

    Добавлен экран Categories с поисковой строкой и fuzzy search (Дамерау-Левентшейн)

---
+++ Financify/Scenes/Categories/CategoriesView.swift
@@ -0,0 +1,49 @@
+import SwiftUI
+
+struct CategoriesView: View {
+    // MARK: - Properties
+    @StateObject private var viewModel: CategoriesViewModel
+    
+    // MARK: - Lifecycle
+    init(categoriesService: CategoriesServiceLogic) {
+        let vm = CategoriesViewModel(
+            categoriesService: categoriesService
+        )
+        _viewModel = StateObject(wrappedValue: vm)
+    }
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                Section(String.categoriesSectionTitle) {
+                    ForEach(viewModel.filteredCategories) { category in
+                        CategoryCell(
+                            category: category
+                        )
+                    }
+                }
+            }
+            .scrollDismissesKeyboard(.immediately)
+            .navigationTitle(String.categoriesTitle)
+            .searchable(
+                text: $viewModel.searchText,
+                placement: .navigationBarDrawer(displayMode: .always),
+                prompt: "Поиск статей"
+            ) {
+                // Подсказки при поиске
+                ForEach(viewModel.suggestions, id: \.self) { suggestion in
+                    Text(suggestion).searchCompletion(suggestion)
+                }
+            }
+            .task {
+                await viewModel.fetchCategories()
+            }
+        }
+    }
+}
+
+// MARK: - Constants
+fileprivate extension String {
+    static let categoriesTitle: String = "Мои статьи"
+    static let categoriesSectionTitle: String = "СТАТЬИ"
+}
