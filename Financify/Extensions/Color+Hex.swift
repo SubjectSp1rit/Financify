58a3e9c9f20bb934498c0a643bb7e978c1585fd4
Arseniy <arseniy@MacBook-Air-Arseniy.local>
2025-07-08 21:52:13 +0300

    Исправлен потенциальный race condition в кэше экстеншна Color+Hex: убран кэш ввиду низкой эффективности работы

--- Financify/Extensions/Color+Hex.swift
+++ Financify/Extensions/Color+Hex.swift
@@ -1,26 +1,17 @@
 import SwiftUI
 
-actor ColorHexCache {
-    private var cache: [UInt64: Color] = [:]
-
-    func get(_ hex: UInt64) -> Color? {
-        cache[hex]
-    }
-
-    func set(_ hex: UInt64, color: Color) {
-        cache[hex] = color
-    }
-}
-
 extension Color {
-    private static var syncCache: [UInt64: Color] = [:]
-    private static let colorCache = ColorHexCache()
-
-    init(hex: String, useAlphaIfAvailable: Bool = true) {
+    /// Создает цвет из HEX-строки.
+    ///
+    /// Символ '#' в начале строки игнорируется. Если строка имеет неверный формат, будет возвращен цвет `.clear`.
+    ///
+    /// - Parameters:
+    ///   - hex: Строка с цветом в HEX-формате.
+    ///   - useAlphaIfAvailable: Если `true` (по умолчанию), используется альфа-канал из 8-значной HEX-строки.
+    public init(hex: String, useAlphaIfAvailable: Bool = true) {
         let hexString = hex
             .trimmingCharacters(in: .whitespacesAndNewlines)
-            .trimmingCharacters(in: CharacterSet(charactersIn: "#"))
-            .uppercased()
+            .replacingOccurrences(of: "#", with: "")
 
         var hexNumber: UInt64 = 0
         guard Scanner(string: hexString).scanHexInt64(&hexNumber) else {
@@ -28,45 +19,32 @@ extension Color {
             return
         }
 
-        if let cached = Self.syncCache[hexNumber] {
-            self = cached
-            return
-        }
-
         let r, g, b, a: Double
 
         switch hexString.count {
         case 3:
-            r = Double((hexNumber >> 8) & 0xF) / 15
-            g = Double((hexNumber >> 4) & 0xF) / 15
-            b = Double(hexNumber & 0xF) / 15
+            r = Double((hexNumber & 0xF00) >> 8) / 15.0
+            g = Double((hexNumber & 0x0F0) >> 4) / 15.0
+            b = Double( hexNumber & 0x00F       ) / 15.0
             a = 1.0
 
         case 6:
-            r = Double((hexNumber >> 16) & 0xFF) / 255
-            g = Double((hexNumber >> 8) & 0xFF) / 255
-            b = Double(hexNumber & 0xFF) / 255
+            r = Double((hexNumber & 0xFF0000) >> 16) / 255.0
+            g = Double((hexNumber & 0x00FF00) >> 8 ) / 255.0
+            b = Double( hexNumber & 0x0000FF       ) / 255.0
             a = 1.0
 
         case 8:
-            a = useAlphaIfAvailable ? Double((hexNumber >> 24) & 0xFF) / 255 : 1.0
-            r = Double((hexNumber >> 16) & 0xFF) / 255
-            g = Double((hexNumber >> 8) & 0xFF) / 255
-            b = Double(hexNumber & 0xFF) / 255
-
+            r = Double((hexNumber & 0xFF000000) >> 24) / 255.0
+            g = Double((hexNumber & 0x00FF0000) >> 16) / 255.0
+            b = Double((hexNumber & 0x0000FF00) >> 8 ) / 255.0
+            a = useAlphaIfAvailable ? Double(hexNumber & 0x000000FF) / 255.0 : 1.0
+            
         default:
             self = .clear
             return
         }
 
-        let result = Color(.sRGB, red: r, green: g, blue: b, opacity: a)
-
-        Self.syncCache[hexNumber] = result
-
-        Task {
-            await Self.colorCache.set(hexNumber, color: result)
-        }
-
-        self = result
+        self.init(.sRGB, red: r, green: g, blue: b, opacity: a)
     }
 }
